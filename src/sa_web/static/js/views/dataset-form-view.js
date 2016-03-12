a89edc289a8c11dd9592a34212324789472cb5d7
goldpbear <tcroxson@gmail.com>
2016-03-11 19:21:44 -0800

    add dataset view controller

---
+++ src/sa_web/static/js/views/dataset-form-view.js
@@ -0,0 +1,67 @@
+/*globals _ Spinner Handlebars Backbone jQuery Gatekeeper */
+
+var Shareabouts = Shareabouts || {};
+
+(function(S, $, console){
+  S.DatasetFormView = Backbone.View.extend({
+    // View responsible for the form for adding and editing places.
+    events: {
+      'submit form': 'onSubmit',
+      'change input[type="file"]': 'onInputFileChange',
+      'click .category-btn-label': 'onCategoryChange',
+    },
+    initialize: function(){
+      // TODO: configure this
+      S.TemplateHelpers.overridePlaceTypeConfig(this.options.placeConfig.items,
+        this.options.defaultPlaceTypeName);
+      S.TemplateHelpers.insertInputTypeFlags(this.options.placeConfig.items);
+    },
+    render: function(){
+      // TODO: configure this
+      var data = _.extend({
+        place_config: this.options.placeConfig,
+        // when category is defined, the selected_category property will contain config file details for fields in the given category
+        selected_category: this.options.placeConfig.categories[category],
+        user_token: this.options.userToken,
+        current_user: S.currentUser,
+        category_selected: category_selected || false
+      }, S.stickyFieldValues, this.model.toJSON());
+
+      this.$el.html(Handlebars.templates['dataset-form'](data));
+      return this;
+    },
+    remove: function() {
+      this.unbind();
+    },
+    onError: function(model, res) {
+      // TODO handle model errors!
+      console.log('oh no errors!!', model, res);
+    },
+    // This is called from the app view
+    setLatLng: function(latLng) {
+      this.center = latLng;
+      this.$('.drag-marker-instructions, .drag-marker-warning').addClass('is-visuallyhidden');
+    },
+    setLocation: function(location) {
+      this.location = location;
+    },
+    // Get the attributes from the form
+    getAttrs: function() {
+      var attrs = {},
+          locationAttr = this.options.placeConfig.location_item_name,
+          $form = this.$('form');
+
+      if (this.location && locationAttr) {
+        attrs[locationAttr] = this.location;
+      }
+
+      return attrs;
+    },
+    onCategoryChange: function(evt) {
+      // TODO: load the appropriate place detail view here...
+    },
+    onInputFileChange: function(evt) {
+
+    }
+  });
+}(Shareabouts, jQuery, Shareabouts.Util.console));
